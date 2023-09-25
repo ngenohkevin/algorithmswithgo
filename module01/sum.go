4232d42eedf4e3a22a4e03907c48e82f475708c9
ngenohkevin <ngenohkevin19@gmail.com>
2023-09-25 12:37:32 +0300

    passed sum

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,12 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+
+	j := 0
+
+	for _, i := range numbers {
+		j = j + i
+	}
+
+	return j
 }
