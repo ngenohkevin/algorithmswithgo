b4b3082914434574f506f94c64a91522c0d2bebb
ngenohkevin <ngenohkevin19@gmail.com>
2023-09-25 13:27:05 +0300

    reversed a string

--- module01/reverse.go
+++ module01/reverse.go
@@ -3,9 +3,12 @@ package module01
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
-//   Reverse("cat") => "tac"
-//   Reverse("alphabet") => "tebahpla"
-//
+//	Reverse("cat") => "tac"
+//	Reverse("alphabet") => "tebahpla"
 func Reverse(word string) string {
-	return ""
+	var res string
+	for _, v := range word {
+		res = string(v) + res
+	}
+	return res
 }
