19dd37571aafe5d6629eef4f31338b30a59d1e97
ngenohkevin <ngenohkevin19@gmail.com>
2023-10-02 12:28:58 +0300

    covert  base

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,14 +1,24 @@
 package module01
 
+import "fmt"
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
 //
 // Eg:
 //
-//   DecToBase(14, 16) => "E"
-//   DecToBase(14, 2) => "1110"
-//
+//	DecToBase(14, 16) => "E"
+//	DecToBase(14, 2) => "1110"
 func DecToBase(dec, base int) string {
-	return ""
+
+	var res string
+
+	for dec > 0 {
+		rem := dec % base
+		res = fmt.Sprintf("%d%s", rem, res)
+		dec = dec / base
+	}
+
+	return res
 }
